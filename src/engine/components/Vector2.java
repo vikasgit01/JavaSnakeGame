815ef84c927e26e37e93cea1e5640a0058571efc
vikasgit01 <vikasreddythota97@gmail.com>
2026-05-19 20:26:14 +0530

    commit - added components for the engine

---
+++ src/engine/components/Vector2.java
@@ -0,0 +1,27 @@
+package engine.components;
+
+public class Vector2 {
+
+    private float x;
+    private float y;
+
+    public float getX(){
+        return x;
+    }
+    public float getY(){
+        return y;
+    }
+
+    public void setX(float x){
+        this.x = x;
+    }
+    public void setY(float y){
+        this.y = y;
+    }
+
+    public Vector2() { }
+    public Vector2(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+}
